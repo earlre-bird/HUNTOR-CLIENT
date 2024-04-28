b408aae555661293342f4a388995a75ee1195d48
areumH <94692566+areumH@users.noreply.github.com>
2024-04-28 23:52:56 +0900

    HUNTOR-6 chore: [Client] 폴더 구조 정리

---
+++ frontend/src/pages/Main/MainPage.tsx
@@ -0,0 +1,7 @@
+import styles from '../../styles/Main/_mainPage.module.scss';
+
+const MainPage = () => {
+  return <div className={styles.main_page}>MainPage 메인페이지</div>;
+};
+
+export default MainPage;
