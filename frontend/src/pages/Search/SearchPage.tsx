20f72654580295bd9c0698e1c6f61b907971328d
areumH <94692566+areumH@users.noreply.github.com>
2024-05-18 23:53:54 +0900

    HUNTOR-28 design: [Client] 서치 페이지 UI 구현

--- frontend/src/pages/Search/SearchPage.tsx
+++ frontend/src/pages/Search/SearchPage.tsx
@@ -15,7 +15,44 @@ const SearchPage = () => {
   return (
     <div className={styles.search_page}>
       <Header keyword={searchKeyWord} />
-      {searchKeyWord}
+      <div className={styles.main}>
+        <div className={styles.main_container}>
+          <div className={styles.select_container}>
+            <select>
+              <option>지역</option>
+            </select>
+            <select>
+              <option>분야</option>
+            </select>
+            <select>
+              <option>경력</option>
+            </select>
+          </div>
+
+          <div className={styles.keyword_container}>
+            <div className={styles.search_keyword}>서울 전체</div>
+            <div className={styles.search_keyword}>온라인</div>
+            <div className={styles.search_keyword}>국어</div>
+            <div className={styles.search_keyword}>예술</div>
+          </div>
+
+          <div className={styles.result_container}>
+            <p className={styles.result_text}>과외 검색 결과 3건</p>
+            <div className={styles.list_container}>
+              <div className={styles.list_item}>
+                <div className={styles.left_item}>
+                  <div className={styles.item_title}>
+                    <p>국어</p>
+                    <p>초등 국어 과외</p>
+                  </div>
+                  <p>온라인</p>
+                </div>
+                <button />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
