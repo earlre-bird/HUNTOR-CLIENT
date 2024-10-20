9a2a4c1743f3a50def757d3fd970fc4d1ce3acac
areumH <94692566+areumH@users.noreply.github.com>
2024-10-20 23:56:14 +0900

    chore: 절대 경로 설정

--- src/router.tsx
+++ src/router.tsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter } from 'react-router-dom';
-import MainPage from './pages/MainPage/MainPage';
+import MainPage from '@/pages/MainPage/MainPage';
 
 const router = createBrowserRouter([
   {
